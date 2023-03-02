17496669e0f9f2e063ed9c6bebe87bf22ccb82e8
MrJujek <julian.dworzycki@gmail.com>
2023-03-02 19:20:42 +0000

    feat: :sparkles: show image and filters

--- static/scripts/imgeditor.js
+++ static/scripts/imgeditor.js
@@ -63,3 +63,167 @@ function renameFile() {
     document.body.append(element)
 }
 
+let globalImgPath = ""
+function drawImage(imgPath) {
+    globalImgPath = imgPath
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+
+    canvas.width = canvas.clientWidth
+    canvas.height = canvas.clientHeight
+
+    let img = new Image();
+    img.src = "https://dev.juliandworzycki.pl/showFile/" + imgPath;
+    img.onload = function () {
+        let drawStartX = 0;
+        let drawStartY = 0;
+        let drawWidth = img.width;
+        let drawHeight = img.height;
+
+        if (drawWidth > canvas.width) {
+            let oldWidth = img.width;
+            let oldHeight = img.height;
+            drawWidth = canvas.width;
+            drawHeight = (drawWidth * oldHeight) / oldWidth;
+        }
+        if (drawHeight > canvas.height) {
+            let oldWidth = img.width;
+            let oldHeight = img.height;
+            drawHeight = canvas.height;
+            drawWidth = (drawHeight * oldWidth) / oldHeight;
+        }
+        drawStartX = (canvas.width - drawWidth) / 2;
+        drawStartY = (canvas.height - drawHeight) / 2;
+
+        ctx.drawImage(img, 0, 0, img.width, img.height, drawStartX, drawStartY, drawWidth, drawHeight);
+    }
+}
+
+function filter(filter, canvasId) {
+    console.log("filter");
+    console.log("filter: ", filter);
+    console.log("canvasId: ", canvasId);
+    let imgPath = globalImgPath
+    //drawImage(imgPath)
+
+    let canvas
+    if (canvasId != undefined) {
+        canvas = document.getElementById(canvasId);
+    } else {
+        canvas = document.getElementById('canvas');
+    }
+    let ctx = canvas.getContext('2d');
+    console.log("ctx: ", ctx);
+
+    switch (filter) {
+        case "grayscale":
+            let imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+            let pixels = imgData.data;
+            for (let i = 0; i < pixels.length; i += 4) {
+
+                let lightness = parseInt((pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3);
+
+                pixels[i] = lightness;
+                pixels[i + 1] = lightness;
+                pixels[i + 2] = lightness;
+            }
+            ctx.putImageData(imgData, 0, 0);
+            break;
+
+        case "invert":
+            let imgData2 = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+            let pixels2 = imgData2.data;
+            for (let i = 0; i < pixels2.length; i += 4) {
+
+                pixels2[i] = 255 - pixels2[i];
+                pixels2[i + 1] = 255 - pixels2[i + 1];
+                pixels2[i + 2] = 255 - pixels2[i + 2];
+            }
+            ctx.putImageData(imgData2, 0, 0);
+            break;
+
+        case "sepia":
+            let imgData3 = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+            let pixels3 = imgData3.data;
+            for (let i = 0; i < pixels3.length; i += 4) {
+
+                let lightness = parseInt((pixels3[i] + pixels3[i + 1] + pixels3[i + 2]) / 3);
+
+                pixels3[i] = lightness + 100;
+                pixels3[i + 1] = lightness + 50;
+                pixels3[i + 2] = lightness;
+            }
+            ctx.putImageData(imgData3, 0, 0);
+            break;
+    }
+}
+
+function drawFilters() {
+    console.log("drawFilters");
+    let imgPath = globalImgPath
+    let canvas = document.getElementById('grayscale');
+    let ctx = canvas.getContext('2d');
+    canvas.width = canvas.clientWidth
+    canvas.height = canvas.clientHeight
+
+    let img = new Image();
+    img.src = "https://dev.juliandworzycki.pl/showFile/" + imgPath;
+    img.onload = function () {
+        let drawStartX = 0;
+        let drawStartY = 0;
+        let drawWidth = img.width;
+        let drawHeight = img.height;
+
+        if (drawWidth > canvas.width) {
+            let oldWidth = img.width;
+            let oldHeight = img.height;
+            drawWidth = canvas.width;
+            drawHeight = (drawWidth * oldHeight) / oldWidth;
+        }
+        if (drawHeight > canvas.height) {
+            let oldWidth = img.width;
+            let oldHeight = img.height;
+            drawHeight = canvas.height;
+            drawWidth = (drawHeight * oldWidth) / oldHeight;
+        }
+        drawStartX = (canvas.width - drawWidth) / 2;
+        drawStartY = (canvas.height - drawHeight) / 2;
+
+        ctx.drawImage(img, 0, 0, img.width, img.height, drawStartX, drawStartY, drawWidth, drawHeight);
+
+        filter("grayscale", "grayscale")
+    }
+
+    canvas = document.getElementById('invert');
+    ctx = canvas.getContext('2d');
+    canvas.width = canvas.clientWidth
+    canvas.height = canvas.clientHeight
+
+    img = new Image();
+    img.src = "https://dev.juliandworzycki.pl/showFile/" + imgPath;
+    img.onload = function () {
+        let drawStartX = 0;
+        let drawStartY = 0;
+        let drawWidth = img.width;
+        let drawHeight = img.height;
+
+        if (drawWidth > canvas.width) {
+            let oldWidth = img.width;
+            let oldHeight = img.height;
+            drawWidth = canvas.width;
+            drawHeight = (drawWidth * oldHeight) / oldWidth;
+        }
+        if (drawHeight > canvas.height) {
+            let oldWidth = img.width;
+            let oldHeight = img.height;
+            drawHeight = canvas.height;
+            drawWidth = (drawHeight * oldWidth) / oldHeight;
+        }
+        drawStartX = (canvas.width - drawWidth) / 2;
+        drawStartY = (canvas.height - drawHeight) / 2;
+
+        ctx.drawImage(img, 0, 0, img.width, img.height, drawStartX, drawStartY, drawWidth, drawHeight);
+
+        filter("invert", "invert")
+    }
+}
